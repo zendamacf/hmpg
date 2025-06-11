9a124287426633d296d2d9ba136ae6bf0143a5a6
Zach Lang <zach.d.lang@gmail.com>
2025-06-11 20:53:37 +1200

    Updates prettier

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,12 @@
-import { sqliteTable, integer } from 'drizzle-orm/sqlite-core';
+import { integer, numeric, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
-export const user = sqliteTable('user', {
-	id: integer('id').primaryKey(),
-	age: integer('age')
+export const image = sqliteTable('image', {
+  id: integer('id').primaryKey(),
+  unsplashid: text('unspashid').unique(),
+  latitude: numeric('latitude'),
+  longitude: numeric('longitude'),
+  location: text('location'),
+  author_name: text('author_name'),
+  author_instagram: text('author_instagram'),
+  url: text('url'),
 });
