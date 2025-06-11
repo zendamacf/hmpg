bf3a486909ad87736b6346f573c5eb7cc671ef41
Zach Lang <zach.d.lang@gmail.com>
2025-06-11 20:53:28 +1200

    More changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script lang="ts">
+  import type { PageProps } from './$types';
+
+  let { data }: PageProps = $props();
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>{data.location ?? 'foobar'}</p>
