3e17b9fd640112d9e3a4212886fdfe4dd0456cf1
Zach Lang <zach.d.lang@gmail.com>
2026-05-25 23:03:00 +1200

    Adds tests and more thorough GH actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
 import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [
-    sentrySvelteKit({
-      sourceMapsUploadOptions: {
-        org: 'kalopsiadev',
-        project: 'hmpg',
-      },
-    }),
+    ...(process.env.VITEST
+      ? []
+      : [
+          sentrySvelteKit({
+            sourceMapsUploadOptions: {
+              org: 'kalopsiadev',
+              project: 'hmpg',
+            },
+          }),
+        ]),
     sveltekit(),
   ],
+  test: {
+    environment: 'node',
+    include: ['src/**/*.{test,spec}.{js,ts}'],
+    exclude: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+  },
 });
