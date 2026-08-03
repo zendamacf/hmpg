b05fea4e5d8fa361348c1464098bc9ea17d75e78
Zach Lang <zach.d.lang@gmail.com>
2026-08-03 22:37:46 +1200

    Updates initial page load to fetch image

---
+++ .changeset/green-wombats-swim.md
@@ -0,0 +1,5 @@
+---
+"hmpg": patch
+---
+
+Updates initial page load to fetch an image if the database has no images.
--- src/routes/refresh/+server.ts
+++ src/routes/refresh/+server.ts
@@ -1,22 +1,6 @@
 import { env } from '$env/dynamic/private';
-import { db } from '$lib/server/db';
-import { image } from '$lib/server/db/schema';
-import { UnsplashAPI } from '$lib/server/unsplash';
+import { refreshImage } from '$lib/server/refresh-image';
 import type { RequestHandler } from './$types';
-
-const KEYWORDS = [
-  'landscape',
-  'water',
-  'aerial',
-  'places',
-  'city',
-  'sunset',
-  'flowers',
-  'snow',
-  'temple',
-  'mountain',
-  'wanderlust',
-];
 
 export const GET: RequestHandler = async ({ request }) => {
   const secret = env.CRON_SECRET;
@@ -27,19 +11,6 @@ export const GET: RequestHandler = async ({ request }) => {
     return new Response(null, { status: 401 });
   }
 
-  const photo = await UnsplashAPI.getRandom(KEYWORDS);
-
-  await db
-    .insert(image)
-    .values({
-      unsplashid: photo.id,
-      latitude: photo.location.latitude?.toString(),
-      longitude: photo.location.longitude?.toString(),
-      location: photo.location.name,
-      author_name: photo.author.name,
-      author_instagram: photo.author.instagram,
-      url: photo.urls.full,
-    })
-    .onConflictDoNothing({ target: image.unsplashid });
+  await refreshImage();
   return new Response();
 };
